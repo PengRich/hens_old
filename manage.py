b35602067f9a5e699c1955da24fa4c85dbb7590c
richair <richest0320@gmail.com>
2017-05-09 22:43:59 +0800

    backup

---
+++ manage.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+# coding=utf-8
+import argparse
+
+from py.utility import Util, ShowAnything
+
+
+class management(object):
+
+    def __init__(self):
+        self.sh = ShowAnything()
+
+    def run(self):
+
+        parser = argparse.ArgumentParser(description="Manage file")
+        add    = parser.add_argument
+
+        # list folders and json files
+        add("-l", "--List", nargs="?", help="list folders and files")
+
+        args   = parser.parse_args()
+        if args.List: self.sh.list_obj(args.List)
+
+
+if __name__ == "__main__":
+    management().run()
