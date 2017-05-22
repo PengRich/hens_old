0fb19e09959251519f1092aa8d8b82158ef06ebe
richair <richest0320@gmail.com>
2017-05-23 07:42:19 +0800

    rename simulator

--- f90/simulator/without_split.f90
+++ f90/simulator/without_split.f90
@@ -2,63 +2,70 @@ module simulator_without_split
   use hens_def
   use flexible_utility
   implicit none
+  ! integer(kind=4), target, private :: he_size(n_hs+n_cs)=0
+  ! integer(kind=4), pointer, private :: h, c 
+  integer(kind=4), private :: he_size(n_hs+n_cs)=0
+  type(exchanger), private :: involved_hes(n_hs+n_cs, n_he)
+  type(exchanger), allocatable, private :: internal_hes(:)
+
   contains
     !***************************************************************
     ! calculate inlet and outlet temperature of each heat exchanger
     !***************************************************************
     subroutine cal_unit_temp
       implicit none
+      integer(kind=4) :: ite=1
 
-      do k=1, n_st
-        do i=1, n_hs
-          do j=1, n_cs
+      allocate(internal_hes(sum(he_size)/2))
 
-            id = (k-1)*n_hs*n_cs + (i-1)*n_cs + j
+      do i=1, n_hs
 
-            if(j .eq. 1) then
-              if(k .ne. 1) then
-                hes(i,j,k)%T_hin = hes(i,n_cs,k-1)%T_hout
-              else
-                hes(i,j,k)%T_hin = streams(i)%T_in 
-              endif
-            else
-              hes(i,j,k)%T_hin = hes(i,j-1,k)%T_hout
-            endif
-            hes(i,j,k)%T_hout = hes(i,j,k)%T_hin - Q_hes(id)/hes(i,j,k)%hs%HCpF
+        if(he_size(i).eq.0) cycle
 
+        involved_hes(i, 1)%T_hin  = streams(i)%T_in
+        involved_hes(i, 1)%T_hout = involved_hes(i, 1)%T_hin - &
+            involved_hes(i, 1)%Q/involved_hes(i, 1)%hs%HCpF
+        internal_hes(ite) = involved_hes(i, 1)
+        ite = ite+1
 
-            if(abs(Q_hes(id)) .gt. 1.d-3) then
-              hes(i,j,k)%ex = 1
-              hes(i,j,k)%Q  = Q_hes(id)
-            else
-              hes(i,j,k)%ex = 0
-              hes(i,j,k)%Q  = 0.d0
-            endif
-            hes(i,j,k)%A_he  = 0.d0 
-            hes(i,j,k)%A_hu  = 0.d0 
-            hes(i,j,k)%A_cu  = 0.d0 
+        if(he_size(i).eq.1) cycle
 
-          enddo
+        do k=2, he_size(i) 
+          involved_hes(i, k)%T_hin  = involved_hes(i, k-1)%T_hout
+          involved_hes(i, k)%T_hout = involved_hes(i, k)%T_hin - &
+              involved_hes(i, k)%Q/involved_hes(i, k)%hs%HCpF
+          internal_hes(ite) = involved_hes(i, k)
+          ite = ite+1
         enddo
       enddo
 
-      do k=n_st, 1, -1
-        do i=n_hs, 1, -1
-          do j=n_cs, 1, -1
+      do j=1, n_cs
 
-            id = (k-1)*n_hs*n_cs + (i-1)*n_cs + j
+        i = n_hs+j
+        if(he_size(i).eq.0) cycle
 
-            if(i .eq. n_hs) then
-              if(k .ne. n_st) then
-                hes(i,j,k)%T_cin = hes(1,j,k+1)%T_cout
-              else
-                hes(i,j,k)%T_cin = streams(n_hs+j)%T_in
-              endif
-            else
-                hes(i,j,k)%T_cin = hes(i+1,j,k)%T_cout
+        involved_hes(i, he_size(i))%T_cin  = streams(i)%T_in
+        involved_hes(i, he_size(i))%T_cout = involved_hes(i, he_size(i))%T_cin + &
+            involved_hes(i, he_size(i))%Q/involved_hes(i, he_size(i))%cs%HCpF
+
+        if(he_size(i).eq.1) cycle
+
+        do k=he_size(i)-1, 1, -1
+          involved_hes(i, k)%T_cin  = involved_hes(i, k+1)%T_cout
+          involved_hes(i, k)%T_cout = involved_hes(i, k)%T_cin + &
+              involved_hes(i, k)%Q/involved_hes(i, k)%cs%HCpF
+        enddo
+      enddo
+
+      do j=1, n_cs
+        i = n_hs+j
+        if(he_size(i).eq.0) cycle
+        do k=1, he_size(i)
+          do id=1, sum(he_size)/2
+            if(internal_hes(id)%tag .eq. involved_hes(i, k)%tag) then
+              internal_hes(id)%T_cin = involved_hes(i, k)%T_cin
+              internal_hes(id)%T_cout = involved_hes(i, k)%T_cout
             endif
-            hes(i,j,k)%T_cout = hes(i,j,k)%T_cin + Q_hes(id)/hes(i,j,k)%cs%HCpF
-                
           enddo
         enddo
       enddo
@@ -74,18 +81,11 @@ module simulator_without_split
 
       call cal_unit_temp
 
-      do k=1, n_st
-        do i=1, n_hs
-          do j=1, n_cs
-
-            if(hes(i,j,k)%ex .eq. 0) cycle
-
-            hes(i,j,k) = flexible_he(hes(i,j,k))
-
-          enddo
-        enddo
+      do k=1, sum(he_size)/2 
+        internal_hes(k) = flexible_he(internal_hes(k))
       enddo
 
+      return
     end subroutine cal_internal_area_utility
 
     !*************************************
@@ -99,8 +99,13 @@ module simulator_without_split
       call cal_internal_area_utility
 
       do i=1, n_hs
-        utilities(i)%T_in = hes(i, n_cs, n_st)%T_hout
+        if(he_size(i).eq.0) then
+          utilities(i)%T_in = streams(i)%T_in
+        else
+          utilities(i)%T_in = involved_hes(i, he_size(i))%T_hout
+        endif
         Q = (utilities(i)%T_in-utilities(i)%T_out)*utilities(i)%s%HCpF
+
         if(abs(Q) .gt. 1.d-3) then 
           utilities(i)%ex = 1
         else
@@ -111,16 +116,22 @@ module simulator_without_split
       enddo
 
       do j=1, n_cs
-        k = n_hs+j
-        utilities(k)%T_in = hes(1, j, 1)%T_cout
-        Q = (utilities(k)%T_in-utilities(k)%T_out)*utilities(k)%s%HCpF
-        if(abs(Q) .gt. 1.d-3) then 
-          utilities(k)%ex = 1
+        i = n_hs + j
+        if(he_size(i).eq.0) then
+          utilities(i)%T_in = streams(i)%T_in
         else
-          utilities(k)%ex = 0
+          utilities(i)%T_in = involved_hes(i, 1)%T_cout
         endif
-        utilities(k)%Q  = Q
-        utilities(k)%A  = 0.d0 
+        write(24,*) i, utilities(i)%T_in
+        Q = (utilities(i)%T_in-utilities(i)%T_out)*utilities(i)%s%HCpF
+
+        if(abs(Q) .gt. 1.d-3) then 
+          utilities(i)%ex = 1
+        else
+          utilities(i)%ex = 0
+        endif
+        utilities(i)%Q  = Q
+        utilities(i)%A  = 0.d0 
       enddo
 
       do k=1, n_hs+n_cs
@@ -162,38 +173,73 @@ module simulator_without_split
       print *, "cal tac"
     end subroutine cal_tac
 
-    real(kind=8) function tac()
+    subroutine init_tac(Qs)
       implicit none
-
+      real(kind=8) :: Qs(n_he)
+        
       results%ac  = 0.d0
       results%huc = 0.d0
       results%cuc = 0.d0
       results%tac = 0.d0
       results%pen = global_pen 
 
-      call cal_terminal_area_utility
-
       do k=1, n_st
         do i=1, n_hs
           do j=1, n_cs
 
-            if(hes(i,j,k)%ex .eq. 0) cycle
-            
-            if(hes(i,j,k)%A_he .gt. 1.d-3) then
-              results%ac = results%ac + expen%fix_A + expen%fac_A*hes(i,j,k)%A_he**expen%ex_A
-            else
-              if(hes(i,j,k)%A_hu .gt. 1.d-3) then
-                Q_hu(1) = Q_hu(1) + abs(hes(i,j,k)%Q)
-                results%ac = results%ac + expen%fix_A + expen%fac_A*hes(i,j,k)%A_hu**expen%ex_A
-              endif
-              if(hes(i,j,k)%A_cu .gt. 1.d-3) then
-                Q_cu(1) = Q_cu(1) + abs(hes(i,j,k)%Q)
-                results%ac = results%ac + expen%fix_A + expen%fac_A*hes(i,j,k)%A_cu**expen%ex_A
-              endif
-            endif
+            id = (k-1)*n_hs*n_cs + (i-1)*n_cs + j
+
+            if(abs(Qs(id)) .lt. 1.d-3) cycle 
+              hes(i,j,k)%ex = 1
+              hes(i,j,k)%Q  = Qs(id)
+              hes(i,j,k)%A_he  = 0.d0 
+              hes(i,j,k)%A_hu  = 0.d0 
+              hes(i,j,k)%A_cu  = 0.d0 
+
+
+              he_size(i)      = he_size(i) + 1
+              he_size(n_hs+j) = he_size(n_hs+j) + 1
+              ! h => he_size(i)
+              ! c => he_size(n_hs+j)
+
+              involved_hes(i, he_size(i))       = hes(i,j,k)
+              involved_hes(i, he_size(i))%tag = id 
+              involved_hes(n_hs+j, he_size(n_hs+j))     = hes(i,j,k)
+              involved_hes(n_hs+j, he_size(n_hs+j))%tag = id 
 
           enddo
         enddo
+      enddo
+
+      return
+
+    end subroutine init_tac
+
+    real(kind=8) function tac(Qs)
+      implicit none
+      real(kind=8) :: Qs(n_he)
+      
+      call init_tac(Qs)
+      call cal_terminal_area_utility
+
+      do k=1, sum(he_size)/2 
+
+        if(internal_hes(k)%A_he .gt. 1.d-3) then
+          results%ac = results%ac + expen%fix_A + &
+              expen%fac_A*internal_hes(k)%A_he**expen%ex_A
+        else
+          if(internal_hes(k)%A_hu .gt. 1.d-3) then
+            Q_hu(1) = Q_hu(1) + abs(internal_hes(k)%Q)
+            results%ac = results%ac + expen%fix_A + &
+                expen%fac_A*internal_hes(k)%A_hu**expen%ex_A
+          endif
+          if(internal_hes(k)%A_cu .gt. 1.d-3) then
+            Q_cu(1) = Q_cu(1) + abs(internal_hes(k)%Q)
+            results%ac = results%ac + expen%fix_A + &
+                expen%fac_A*internal_hes(k)%A_cu**expen%ex_A
+          endif
+        endif
+
       enddo
 
       do k=1, n_hs+n_cs
