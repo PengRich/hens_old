08765c675b9b0a9ca3c59606bd7ea66c652f78d5
richair <richest0320@gmail.com>
2017-05-24 22:22:03 +0800

    use pointer

--- f90/def.f90
+++ f90/def.f90
@@ -11,28 +11,34 @@ module hens_def
   implicit none
   character(len=100), public, parameter :: case_name = "case_9sp1"
   integer(kind=4),    public, parameter :: n_hs=4, n_cs=5, n_st=3, n_hu=1, n_cu=1
-  integer(kind=4),    public, parameter :: n_he=n_hs*n_cs*n_st
+  integer(kind=4),    public, parameter :: n_he=n_hs*n_cs*n_st, n_s=n_hs+n_cs
 
   type(stream),    public :: streams(n_hs+n_cs+n_hu+n_cu)
   type(expense),   public :: expen
   type(cost),      public :: results 
-  type(exchanger), public :: hes(n_hs, n_cs, n_st)
-  type(utility),   public :: utilities(n_hs+n_cs)
+  real(kind=8),    public :: Q_hes(n_he)=0.d0
+  real(kind=8),    public :: Q_hu(n_hu)=0.d0, Q_cu(n_cu)=0.d0
 
-  real(kind=8), public :: Q_hes(n_he)=0.d0
-  real(kind=8), public :: Q_hu(n_hu)=0.d0, Q_cu(n_cu)=0.d0
+  type(exchanger), target,  public :: hes(n_cs, n_hs, n_st)
+  type(utility),   target,  public :: utilities(n_hs+n_cs)
+  type(exchanger), pointer, public :: hesp(:)
+  type(utility),   pointer, public :: utip(:)
 
   contains
     subroutine init 
       implicit none
 
       select case(case_name)
+
         case("case_9sp1") 
           include "case_9sp1.inc"
+
         case("case_10sp2")
           include "case_10sp2.inc"
+
         case default
           call logger("Warning: Input wrong case parameters!")
+
       end select
 
       return
--- f90/utils.f90
+++ f90/utils.f90
@@ -1,19 +1,34 @@
 module util
   implicit none
+  interface logger
+    module procedure log_char
+    module procedure log_real
+  end interface logger
   contains
-    subroutine logger(message)
+
+    subroutine log_char(message)
+      implicit none
       character(len=*) :: message
       
       write(*, *) message
       
-    end subroutine logger
+    end subroutine log_char
+
+    subroutine log_real(message)
+      real(kind=8) :: message
+      
+      write(*, *) message
+      
+    end subroutine log_real
+
 end module util
         
 module penalty
   implicit none
   real(kind=8), parameter :: pen_factor=1.d7
-  real(kind=8) :: global_pen = 0.d0
+  real(kind=8), public    :: global_pen = 0.d0
   contains
+
     subroutine add_penalty(excess)
 
       real(kind=8), intent(in) :: excess
